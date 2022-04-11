29407215dab80d80bea3053dc6a8a950fd862045
GmGith <87491753+GmGith@users.noreply.github.com>
2022-04-11 19:36:24 +0200

    Extraction des liens de catégories via ul et li

--- scrape/scrapeBook.py
+++ scrape/scrapeBook.py
@@ -1,14 +1,22 @@
 #Import requests and csv
 import requests
-import os
 import csv
 from bs4 import BeautifulSoup
 #Access the url of the site
 url = "http://books.toscrape.com/"
 #page = requests.get(url)
-#Telling bs4 to parse the content of the page
+#Dire à bs4 de parser le contenu de la page
 main_page = requests.get(url)   
 soup = BeautifulSoup(main_page.content, 'html.parser')
-print(soup)
+# Récupérer l'ul dans la barre de navigation
+list_first_ul = soup.find('ul', {"class": "nav-list"})
+# Extraire l'ul qui contient la liste
+list_ul = list_first_ul.find('ul')
+# Avec cet élément de liste "li", nous obtenons tous les liens vers les catégories
+links_books_category = list_ul.find_all('li')
+for li in links_books_category:
+    link_category_name = li.find('a')['href'].split('/')[3]
+    category = AllCategory(link_category_name)
+    self.list_of_categories.append(category)
 
 
